f43db46e11a857479f94243457bc8ffedfe84387
DESKTOP-8RQ4NJB\ADMIN <thu.vo@lihuhugames.com>
2023-03-24 11:18:35 +0700

    Random icon to tilemap

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tile : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +17,13 @@ public class Tile : MonoBehaviour
     {
         
     }
+
+
+    
 }
+
+
+
+
+
+
--- Assets/Scripts/Tilemap.cs
+++ Assets/Scripts/Tilemap.cs
@@ -10,10 +10,14 @@ public class Tilemap : MonoBehaviour
 
     float size = 0.9f;
 
-    int row = 5;
-    int col = 3;
+    int row = 10;
+    int col = 6;
     public Tile[,] tilemap;
     public GameObject tilePrefab;
+    public Sprite[] icons;
+
+    public Sprite sprite { get; private set; }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,51 +26,15 @@ public class Tilemap : MonoBehaviour
         {
             for (int j= 0; j<row; j ++)
             {
-                GameObject go = Instantiate(tilePrefab, new Vector3(i*size - ((size*col)/2f)+size/2, j*size - (size*row/2f)+size/2, 0), Quaternion.identity);
+                GameObject go = Instantiate(tilePrefab, new Vector3(i*size - (size*col)/2f + size/2, j*size - (size*row)/2f + size/2, 0), Quaternion.identity);
                 go.name = i + "-" + j;
-                tilemap[j, i] = go.GetComponent<Tile>(); 
+                Sprite icon = icons[Random.Range(0, icons.Length)];
+                go.GetComponent<SpriteRenderer>().sprite = icon;
+                //tilemap[j, i] = go.GetComponent<Tile>();
             }
         }
 
-        /*
-        for (float i = 0; i <= 2; i += 0.9f)
-        {
-            for (float j = 0; j <= 4; j += 0.9f)
-            {
-                GameObject go = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity);
-                go.name = i + "-" + j;
-            }
-        }
-
-        for (float i = 0; i >= -2; i -= 0.9f)
-        {
-            for (float j = 0; j <= 4; j+=0.9f)
-            {
-                GameObject go = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity);
-                go.name = i + "-" + j;
-
-            }
-        }
-
-        for (float i = 0; i <= 2; i += 0.9f)
-        {
-            for (float j = 0; j >= -4; j -=0.9f)
-            {
-                GameObject go = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity);
-                go.name = i + "-" + j;
-
-            }
-        }
-
-        for (float i = 0; i >= -2; i -= 0.9f)
-        {
-            for (float j = 0; j >= -4; j -=0.9f)
-            {
-                GameObject go = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity);
-                go.name = i + "-" + j;
-
-            }
-        }*/
+        
     }
 
     // Update is called once per frame
@@ -81,18 +49,8 @@ public class Tilemap : MonoBehaviour
             if (Physics.Raycast(ray, out hit))
             {
                 Debug.Log(hit.collider.gameObject.name);
+
                 Vector3 position = transform.position;
-
-
-                //Matrix4x4 MVPMatrix = mainCamera.projectionMatrix * mainCamera.worldToCameraMatrix * transform.localToWorldMatrix;
-
-                //// Chuyển đổi tọa độ từ tọa độ Unity sang tọa độ ma trận
-                //Vector4 transformedPosition = MVPMatrix * new Vector4(objectPosition.x, objectPosition.y, objectPosition.z, 1);
-
-                //// Hiển thị tọa độ trong tọa độ ma trận
-                //Debug.Log("Transformed Position: " + transformedPosition);
-
-                
             }
         }
 
